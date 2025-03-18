e07556f608458b1db4d948ea80d28dda4c2ce01e
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-03-19 01:42:58 +0530

    vpc

--- outputs.tf
+++ outputs.tf
@@ -15,9 +15,13 @@ output "public_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet.priavte[*].id
+    value = aws_subnet.private[*].id
 }
 
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
 }
